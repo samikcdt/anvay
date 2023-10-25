0f7abab8eb890ef0d5fdd2f666dc1bfc61ab1b30
Prakash Patil <prakash.patil@cropdata.in>
2023-10-25 11:58:19 +0530

    changes v2

--- js/main.js
+++ js/main.js
@@ -16,11 +16,14 @@
 
     const setTheme = function ( theme )
     {
-        if ( theme === 'auto' && window.matchMedia( '(prefers-color-scheme: dark)' ).matches )
-        {
+        if ( theme === 'auto' && window.matchMedia( '(prefers-color-scheme: dark)' ).matches ){
             document.documentElement.setAttribute( 'data-bs-theme', 'dark' )
-        } else
-        {
+        } else if (
+            theme === "auto" &&
+            window.matchMedia( "(prefers-color-scheme: light)" ).matches
+        ){
+            document.documentElement.setAttribute( "data-bs-theme", "light" );
+        } else {
             document.documentElement.setAttribute( 'data-bs-theme', theme )
         }
     }
